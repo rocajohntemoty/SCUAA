7875de064cf3a07802c948ab3b505c6cb65653bc
roca <rocajohntemoty@yahoo.com>
2013-07-06 17:56:09 +0800

    add points models

--- models/points.php
+++ models/points.php
@@ -10,4 +10,13 @@
 			$school->execute();
 			return $school->fetchAll(PDO::FETCH_ASSOC);
 		}
+		public function create($data){
+			$sql	=	"insert into points(gold,silver,bronze) values(:gold,:silver,:bronze)";
+			$createSchool	=	$this->db->prepare($sql);
+			$createSchool->bindParam(":gold",$data['gold'],PDO::PARAM_INT);
+			$createSchool->bindParam(":silver",$data['silver'],PDO::PARAM_INT);
+			$createSchool->bindParam(":bronze",$data['bronze'],PDO::PARAM_INT);
+			$logs	=	$createSchool->execute();
+			return logs;
+		}
 	}
